f5bf7cd46c4c302365a4af3addc507bedd971428
nkc287dev <nkc287dev@gmail.com>
2025-05-16 16:44:14 +0530

    initial commit

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -8,7 +8,18 @@ const userSchema =  new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        // validate: {
+        //     validator: (value) => {
+        //         return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value);
+        //     },
+        //     message: props => `${props.value} is not a valid email!`
+        // },
+        // match:[
+        //     "/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/",
+        //     "Please fill a valid email address"
+        // ]
     }
 }, { timestamps: true})
 
